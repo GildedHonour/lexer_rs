6c17a7e0ebf67d0a6760392b1b4774ff568c2030
alex maslakoff <alex@serendipia.email>
2020-12-11 11:45:55 +0530

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+const STACK_DEPTH: i32 = 2048;
+const DEFAULT_TEMPLATE_DEPTH: i32 = 65536;
+
+fn parse() {
+
+}
+
+
+fn try_parse_import_statement() {
+
+}
+
+fn try_parse_export_statement() {
+
+}
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
 }
